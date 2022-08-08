1c7e9e2dd348cc77bc9f1dd7bd995d57e302208c
syedgulahmed <syedgulahmed@hotmail.com>
2022-08-08 18:02:31 +0500

    Adding components to main page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
+
 import memories from './images/image.png';
 const App = () => {
     return (
@@ -9,6 +12,18 @@ const App = () => {
                 <Typography varaint="h2" align="center">Memories</Typography>
                 <img src={memories} alt="memories" height="60" />
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />   
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
