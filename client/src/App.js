7ac94ec5540c3ca1e57c9b6e9cce18a512c95930
syedgulahmed <syedgulahmed@hotmail.com>
2022-08-09 19:26:09 +0500

    Adding styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 import Posts from './components/Posts/Posts.js'
 import Form from './components/Form/Form.js'
-
 import memories from './images/image.png';
+import useStyles from './styles';
+
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography varaint="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} varaint="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
